b36fc950c74c4c1aaa304aa1ad60a6357d71728c
= <1151878751@qq.com>
2018-08-04 18:09:34 +0800

    exception

--- exceptions/ServiceException.php
+++ exceptions/ServiceException.php
@@ -9,7 +9,9 @@ class ServiceException extends LogicException
     public function __construct($message)
     {
         if (is_null($message)) {
+
             return;
+
         }
 
         $this->message = $message;
